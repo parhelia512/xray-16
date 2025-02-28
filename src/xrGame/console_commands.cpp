de5ae6a514c8c9b5a07ca540dad1899785cd4873
Xottab-DUTY <xottab-duty@yandex.ru>
2025-03-01 03:34:36 +0500

    Detect if localization.ltx was changed and use the updated language value
    Before introducing a console command to change the language, mods used
    to change localization.ltx. We now detect that and reselect the language
    according to the updated localization.ltx file.
    
    For example, this allows localization switch to work in Call of
    Chernobyl (#1529)

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -244,6 +244,12 @@ public:
     }
 };
 
+class CCC_GameLanguageInLTX : public CCC_String
+{
+public:
+    CCC_GameLanguageInLTX(pcstr name) : CCC_String(name, )
+};
+
 #ifdef DEBUG
 class CCC_ALifePath : public IConsole_Command
 {
@@ -2005,6 +2011,7 @@ void CCC_RegisterCommands()
     CMD3(CCC_Mask, "g_crouch_toggle", &psActorFlags, AF_CROUCH_TOGGLE);
     CMD1(CCC_GameDifficulty, "g_game_difficulty");
     CMD1(CCC_GameLanguage, "g_language");
+    CMD3(CCC_String, "g_language_ltx", CStringTable::LanguageIDInLTX, std::size(CStringTable::LanguageIDInLTX));
 
     CMD3(CCC_Mask, "g_backrun", &psActorFlags, AF_RUN_BACKWARD);
 
